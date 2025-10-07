2bcdc2d25d9625f8894a57527a86684d5defbe37
isadorabello <isadora.bello@gec.inatel.br>
2025-10-07 12:52:37 -0300

    first commit

---
+++ src/main/java/io/github/isadorabello/agendadoreventos/AgendadorEventosApplication.java
@@ -0,0 +1,13 @@
+package io.github.isadorabello.agendadoreventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorEventosApplication.class, args);
+	}
+
+}
