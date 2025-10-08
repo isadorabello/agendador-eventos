46d060d9b24d8d9ed99513d36242d0bfd5b158d6
isadorabello <isadora.bello@gec.inatel.br>
2025-10-08 15:46:49 -0300

    feat: adding mapper configuration

--- src/main/java/io/github/isadorabello/agendadoreventos/business/mapper/EventoMapper.java
+++ src/main/java/io/github/isadorabello/agendadoreventos/business/mapper/EventoMapper.java
@@ -4,16 +4,15 @@ import io.github.isadorabello.agendadoreventos.business.dto.EventoDTO;
 import io.github.isadorabello.agendadoreventos.infrastructure.entity.Evento;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import java.util.List;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
-@Mapper(componentModel = SPRING)
+@Mapper(componentModel = SPRING, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface EventoMapper {
 
-    @Mapping(source = "id", target = "id")
-    @Mapping(source = "dataAlteracao", target = "dataAlteracao")
     Evento paraEventoEntity(EventoDTO dto);
 
     EventoDTO paraEventoDTO(Evento entity);
